88ed30a6f897cf1fab339be76fe42c2fae4ce44b
t.nugent <t.nugent@hotmail.co.uk>
2023-10-04 15:33:47 +0100

    Parses text file. Identifies files, and directories. Populates tree-like structure.

--- Day-7-Directories/Program.cs
+++ Day-7-Directories/Program.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 
 class DirectoryNode
 {
-    string name;
-    bool isFolder;
-    List<string> children;
-    int size;
+    private string name;
+    private bool isFolder;
+    private List<string> children;
+    private int size;
 
     public DirectoryNode(string newName, bool newIsFolder, int newSize = 0)
     {
         name = newName;
         isFolder = newIsFolder;
+        children = new List<string>();  
         size = newSize;
+    }
+
+    public void AddChild(string childName)
+    {
+        children.Add(childName);
+    }
+
+    public int NumberOfChildren()
+    {
+        return children.Count;
+    }
+
+    public List<string> GetChildren()
+    {
+        return children;
     }
 }
 
@@ -22,28 +38,65 @@ class Program
     {
         string[] cmds = System.IO.File.ReadAllLines(@"C:\Users\Tom\Documents\Advent\Day-7-Directories\Day-7-Directories\data_test.txt");
 
-        DirectoryNode home = new DirectoryNode("/", true, 0);
 
-        string currentDir = "";
+        DirectoryNode home = new DirectoryNode("/", true, 0);
+        Dictionary<string, DirectoryNode> fromNameToDirectory = new Dictionary<string, DirectoryNode>();
+
+        // Starting in home directory
+        string currentDir = ""; 
+        DirectoryNode newNode = new DirectoryNode("/", true);
+        fromNameToDirectory.Add("/", newNode); // Create new node in the dictionary
+        
         foreach (string cmd in cmds)
         {
-            if (cmd[0] == '$' && cmd[2] == 'c')
+            if (cmd[0] == '$')
             {
-                // $ cd
-                if (cmd[5] == '.')
+                if (cmd[2] == 'c')
                 {
-                    // $ cd ..
-                    Console.WriteLine("Change out");
+                    // $ cd
+                    if (cmd[5] == '.')
+                    {
+                        // $ cd ..
+                        Console.WriteLine("Change out");
+                    }
+                    else
+                    {
+                        string dir = cmd.Substring(5);
+                        currentDir = dir;
+                        Console.WriteLine(String.Format("Change into {0}", dir));
+                    }
                 }
-                else
+            } else // Creating a text file or a directory
+            {
+                // Creating a directory
+                if (cmd.Substring(0, 3) == "dir")
                 {
-                    string dir = cmd.Substring(5);
-                    Console.WriteLine(String.Format("Change into {0}", dir));
+                    // Making a new directory
+                    string directoryName = cmd.Substring(4); // After "dir " to end is the name
+                    newNode = new DirectoryNode(directoryName, true);
+                    fromNameToDirectory.Add(directoryName, newNode); // Create new node in the dictionary
+                    fromNameToDirectory[currentDir].AddChild(directoryName);
+                } else // Creating a file
+                {
+                    string[] sizeName = cmd.Split(" ");
+                    newNode = new DirectoryNode(sizeName[1], false, Convert.ToInt32(sizeName[0]));
+                    fromNameToDirectory.Add(sizeName[1], newNode); // Create new node in the dictionary
+                    fromNameToDirectory[currentDir].AddChild(sizeName[1]);
                 }
             }
         }
 
-        // CurrentDirectoryStack
+        foreach(KeyValuePair<string, DirectoryNode> pair in fromNameToDirectory)
+        {
+            Console.WriteLine(String.Format("Directory {0} has {1} children", pair.Key, pair.Value.NumberOfChildren()));
+            List<string> children = pair.Value.GetChildren();
+            foreach(string child in children)
+            {
+                Console.WriteLine(String.Format("\t{0}", child));
+            }
+        }
+                
+                // CurrentDirectoryStack
 
         // For each command
 
